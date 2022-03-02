093201909990aa08120c80408a0daef5e8ba7028
Mateus Torquato de Medeiros <mateus.torquato@eposnow.com>
2022-03-02 18:42:45 -0300

    Configuring .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect(process.env.MONGO_DB_URI)
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
